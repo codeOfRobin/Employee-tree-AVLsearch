1e8ec28eee20c8eaadcbbbe9fd4f840c70c4eb7d
codeofRobin <malhotra.robin@icloud.com>
2013-09-30 17:27:13 +0530

    Initial Commit

---
+++ CSL201Assignment4/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CSL201Assignment4
+//
+//  Created by Robin Malhotra on 30/09/13.
+//  Copyright (c) 2013 Robin's code kitchen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
